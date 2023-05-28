d81aa52fdb1413f339ff5584c30b7be3e31a697f
S0UMIM00N <soumiaaboudrar@gmail.com>
2023-05-28 15:09:09 -0700

    printf.c

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * printf - function that produces output according to a format
+ * @format : character string 
+ * owned by karkazou & S0UMIM00N
+ * return : counter of char
+ */
+
+
